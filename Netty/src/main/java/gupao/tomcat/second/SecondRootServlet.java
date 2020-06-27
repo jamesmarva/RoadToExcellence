6566edb94b273416bbed55b21a65d029791eec73
jameskk <jamesmarva@users.noreply.github.com>
2020-06-27 18:59:33 +0800

    增加流的示例增加Netty示例的代码

---
+++ Netty/src/main/java/gupao/tomcat/second/SecondRootServlet.java
@@ -0,0 +1,25 @@
+package gupao.tomcat.second;
+
+import gupao.tomcat.first.FirstRequest;
+import gupao.tomcat.first.FirstResponse;
+
+/**
+ * <功能描述><br>
+ *
+ * @author Brilliant James Jamesmarva1993@gmail.com 2020-06-17 23:35
+ **/
+public abstract class SecondRootServlet {
+
+    public void service(FirstRequest request, FirstResponse response) throws Exception {
+        if ("GET".equalsIgnoreCase(request.getMethod())) {
+            doGet(request, response);
+        } else {
+            doPost(request, response);
+        }
+    }
+
+    public abstract void doGet(FirstRequest request, FirstResponse response) throws Exception;
+
+    public abstract void doPost(FirstRequest request, FirstResponse response) throws Exception;
+
+}
