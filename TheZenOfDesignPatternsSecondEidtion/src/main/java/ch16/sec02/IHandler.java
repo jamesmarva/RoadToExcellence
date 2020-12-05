694473916cf9adbd8bb4047c5a51e42688b79427
jameskk <jamesmarva@163.com>
2020-12-05 22:08:33 +0800

    feat  新增设计模式之禅

---
+++ TheZenOfDesignPatternsSecondEidtion/src/main/java/ch16/sec02/IHandler.java
@@ -0,0 +1,19 @@
+package ch16.sec02;
+
+/**
+ *
+ *
+ * @author 王涵威
+ * @date 20.12.5 20:48
+ */
+public interface IHandler {
+
+
+    /**
+     * handle the women's request message.
+     * @param women be handled.
+     */
+    void handleMessage(IWomen women);
+
+
+}
