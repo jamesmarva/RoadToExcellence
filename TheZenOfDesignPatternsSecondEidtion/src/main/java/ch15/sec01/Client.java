0f7ba7a9b75a972a757cc46ee65a8c1c2695b694
jameskk <jamesmarva@users.noreply.github.com>
2020-12-10 09:26:26 +0800

    feat: chapter 15

---
+++ TheZenOfDesignPatternsSecondEidtion/src/main/java/ch15/sec01/Client.java
@@ -0,0 +1,19 @@
+package ch15.sec01;
+
+import javax.swing.*;
+
+/**
+ * <功能描述><br>
+ *
+ * @author Brilliant James Jamesmarva1993@gmail.com 2020-12-10 09:06
+ **/
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println("---- client require add");
+        Group gp = new RequirementGroup();
+        gp.find();
+        gp.add();
+        gp.plan();
+    }
+}
