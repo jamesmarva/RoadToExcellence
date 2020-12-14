a2f4b4271ce677268664ab64ecf7da44aeef6b0a
jameskk <jamesmarva@users.noreply.github.com>
2020-12-14 08:34:56 +0800

    feat: add chapter 20

---
+++ TheZenOfDesignPatternsSecondEidtion/src/main/java/ch35/sec1/Card.java
@@ -0,0 +1,18 @@
+package ch35.sec1;
+
+import lombok.Data;
+
+/**
+ * 代码清单35-1 IC卡类
+ *
+ * @author Brilliant James Jamesmarva1993@gmail.com 2020-12-14 06:41
+ **/
+@Data
+public class Card {
+
+    private String cardNo;
+
+    private int steadyMoney;
+
+
+}
