6cca86a2218fca7874c5264116cdbf5e5ff617fd
jameskk <jamesmarva@users.noreply.github.com>
2020-12-21 08:33:33 +0800

    feat: add security web

---
+++ WebSecurityDemo0/src/main/java/com/hungry/security/Application.java
@@ -0,0 +1,15 @@
+package com.hungry.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Brilliant James Jamesmarva1993@gmail.com 2020-12-20 13:09
+ **/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
